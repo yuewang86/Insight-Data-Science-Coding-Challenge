b07d077c77cef7b09d03d16ac5b764dd63199add
yuewang86 <yw1228@nyu.edu>
2015-07-09 12:36:48 -0400

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,5 +1,4 @@
 
 #!/usr/bin/env bash
 
-java ./src/words_tweeted.java ./tweet_input/tweets.txt ./tweet_output/ft1.txt
-java ./src/median_unique.java ./tweet_input/tweets.txt ./tweet_output/ft2.txt
+java ./src/test.java 
