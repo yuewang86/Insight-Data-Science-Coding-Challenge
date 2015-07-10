56211b4f52c9482262c0b4151fbd5f4a75cda836
yuewang86 <yw1228@nyu.edu>
2015-07-09 22:05:31 -0400

    Update run.sh

--- run.sh
+++ run.sh
@@ -2,7 +2,7 @@
 #!/usr/bin/env bash
 
 javac ./src/words_tweeted.java
-java ./src/word_tweeted.class
+java ./src/words_tweeted.class
 
 javac ./src/median_unique.java
 java ./src/median_unique.class
